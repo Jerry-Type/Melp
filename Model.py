1016e50f95709bf768500d795761bcdc9dfb70a5
Jerry-Type <gerry_math@comunidad.unam.mx>
2018-09-27 13:50:17 -0500

    Update the model

--- Model.py
+++ Model.py
@@ -11,24 +11,44 @@ class Restaurant(db.Model):
     #Partial implementation
     __tablename__ = 'restaurants'
     __table_args__ = {"schema":"melp"}
-    id = db.Column(db.String(250), primary_key=True)
+    id = db.Column(db.Text, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(250),nullable=False)
-    email = db.Column(db.String(250),nullable=False)
+    name = db.Column(db.Text,nullable=False)
+    site = db.Column(db.Text,nullable=False)
+    email = db.Column(db.Text,nullable=False)
     phone = db.Column(db.Text,nullable=False)
+    street = db.Column(db.Text,nullable=False)
+    city = db.Column(db.Text,nullable=False)
+    state = db.Column(db.Text,nullable=False)
+    lat = db.Column(db.Float,nullable=False)
+    lng = db.Column(db.Float,nullable=False)
+    
+
    
-    def __init__(self, id, rating,name,email,phone):
+    def __init__(self, id, rating,name,site,email,phone,street,city,state,lat,lng):
         self.id = id
         self.rating = rating
         self.name = name
+        self.site = site
         self.email = email
         self.phone = phone
+        self.street = street
+        self.city = city
+        self.state = state
+        self.lat = lat
+        self.lng = lng
 
 class RestaurantSchema(ma.Schema):
     #Partial implementation
     id = fields.String(required=True)
-    rating = fields.Integer(required=True)
-    name = fields.String(required=True)
-    email = fields.String(required=True)
-    phone = fields.String(required=True)
+    rating = fields.Integer(required=False)
+    name = fields.String(required=False)
+    site = fields.String(required=False)
+    email = fields.String(required=False)
+    phone = fields.String(required=False)
+    street = fields.String(required=False)
+    city = fields.String(required=False)
+    state = fields.String(required=False)
+    lat = fields.Float(required=False)
+    lng = fields.Float(required=False)
 
