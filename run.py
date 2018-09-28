9ab6828e5e1da98a5bbef42889651774c01f5913
Jerry-Type <gerry_math@comunidad.unam.mx>
2018-09-27 20:33:29 -0500

    Some changes to run

--- run.py
+++ run.py
@@ -1,20 +1,16 @@
 from flask import Flask
+from app import api_bp
+from Model import db
 
+#def create_app(config_filename):
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
-    
-    from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-
-    from Model import db
-    db.init_app(app)
-
-    return app
+myapp = Flask(__name__)
+myapp.config.from_object("config")
+myapp.register_blueprint(api_bp, url_prefix='/api')
+db.init_app(myapp)
 
 
 if __name__ == "__main__":
-    app = create_app("config")
-    app.run(debug=True)
+    #myapp = create_app("config")
+    myapp.run(debug=True)
 
