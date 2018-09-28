3e70789ad71cd662355003150ea4cc75ab15b281
Jerry-Type <gerry_math@comunidad.unam.mx>
2018-09-27 19:02:03 -0500

    Structure of the app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Restaurante import Restaurante
+from resources.Statistics import Statistics
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 api.add_resource(Restaurante,'/restaurant')
+api.add_resource(Statistics,'/restaurant/statistics')
