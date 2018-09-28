9af01c2c7cc9e69ce0ded4a48774aa9e651adecc
Jerry-Type <gerry_math@comunidad.unam.mx>
2018-09-27 20:38:55 -0500

    Config first DB

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ from flask import Flask
 from app import api_bp
 from Model import db
 
-#def create_app(config_filename):
-
 myapp = Flask(__name__)
 myapp.config.from_object("config")
 myapp.register_blueprint(api_bp, url_prefix='/api')
@@ -11,6 +9,5 @@ db.init_app(myapp)
 
 
 if __name__ == "__main__":
-    #myapp = create_app("config")
     myapp.run(debug=True)
 
