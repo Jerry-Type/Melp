ef449fc95a33a87eefebe84c61e2d9341cc6706d
Jerry-Type <gerry_math@comunidad.unam.mx>
2018-09-27 19:03:34 -0500

    A little change to URI

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = #Add your URI :)
+SQLALCHEMY_DATABASE_URI = "postgres://ezztoeklbecqcq:62741b27fe0cd501ea335d919ebc9ce6ab4a9cc769a4ca8b24636ba08971c22b@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dcmdsmm63u6829" #Add your URI :)
