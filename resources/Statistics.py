b358cc67dcae4b58bf1697305bc92324b6c8056d
Jerry-Type <gerry_math@comunidad.unam.mx>
2018-09-28 09:22:38 -0500

    Add some comments to the method and delete some garbage lines

--- resources/Statistics.py
+++ resources/Statistics.py
@@ -12,11 +12,10 @@ parser = reqparse.RequestParser()
 
 class Statistics(Resource):
     def get(self):
+        """This method returns some statistics of the restaurants that are within a circle with parameters (x(long),y(lat),z,(radius))"""
         args = request.args
-        #print (args['latitude'])
         #set_query = str("SELECT * FROM melp.restaurants as A WHERE ST_PointInsideCircle(ST_Point(A.lng,A.lat),"+args['longitude']+","+args['latitude']+","+args['radius']+")")
         set_query = str("SELECT * FROM melp.restaurants as A WHERE ST_DWithin(ST_MakePoint(A.lng,A.lat), ST_MakePoint("+args['longitude']+","+args['latitude']+")::geography,"+args['radius']+")")  
-        #print (set_query)
         restaurantes = Restaurant.query.from_statement(text(set_query))
         restaurantes = restaurantes_schema.dump(restaurantes).data
         number_restaurants = len(restaurantes)
@@ -25,5 +24,4 @@ class Statistics(Resource):
            desv_rating= math.sqrt(sum([(restaurant['rating']-avg_rating)**2 for restaurant in restaurantes ])/(len(restaurantes)-1.0))
         else:
             desv_rating = 0.0
-        #print (number_restaurants,avg_rating,desv_rating)
         return { "status": 'success', 'data': {'count':number_restaurants,'avg':avg_rating,'std':desv_rating} }, 201
